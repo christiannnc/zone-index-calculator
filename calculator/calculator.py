e1237bf1da59a1d7d47a5e4c0ae3fd1bfb274d9e
christiannnc <c917unc@gmail.com>
2025-03-08 15:45:00 -0500

    initial commit

---
+++ calculator/calculator.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import json
+from cities import LOW, MED, HIGH, ALT_HIGH, initalize
+from typing import Dict
+
+ACRE_MULTIPLE = 43560
+DX_RESIDENTIAL_RATIO = 0.152
+
+WEIGHTS = {
+    LOW: 0.1,
+    MED: 0.3,
+    HIGH: 0.9
+}
+
+def process_zones(data: pd.DataFrame, categories: Dict, requires_area_conversion: bool, area_key: str, code_key: str) -> float:
+    total_residential_area = 0
+
+    for _, row in data.iterrows():
+        area = 0
+        if requires_area_conversion:
+            area = row[area_key] / ACRE_MULTIPLE
+        else:
+            area = row[area_key]
+        code = row[code_key]
+        
+
+        if code in categories[LOW]['codes']:
+            categories[LOW]['area'] += area
+            total_residential_area += area
+        elif code in categories[MED]['codes']:
+            categories[MED]['area'] += area
+            total_residential_area += area
+        elif code in categories[HIGH]['codes'] or code.startswith(categories[ALT_HIGH]):
+            transformed_area = area
+            if code.startswith(categories[ALT_HIGH]):
+                transformed_area *= DX_RESIDENTIAL_RATIO
+            categories[HIGH]['area'] += transformed_area
+            total_residential_area += transformed_area
+
+    return total_residential_area
+
+def calculate_distributions(densities: Dict, total_area: float):
+    for density_label, density in densities.items():
+        if density_label == ALT_HIGH:
+            continue
+        density['dist'] = density['area'] / total_area
+
+def calculate_zone_index(population: float, categories: Dict, weights: Dict) -> float:
+    weighted_sum = sum(
+        weights[key] * categories[key]['dist'] 
+        for key in categories if key != ALT_HIGH
+    ) * 1e5
+    return population / weighted_sum
+
+def format_density_distribution(city):
+    distributions = { LOW: {}, MED: {}, HIGH: {} }
+
+    for key in city['densities']:
+        if key == ALT_HIGH:
+            continue
+
+        distributions[key] = {
+            'area': round(city['densities'][key]['area'], 2),
+            'share': round(city['densities'][key]['dist'], 4)
+        }
+
+    return distributions
+
+def main():
+    print('calculating...')
+    initialized_cities = initalize()
+
+    for key in initialized_cities:
+        data = pd.read_csv(f"./data/{key}_zoning.csv")
+        drop_key = initialized_cities[key]['drop']
+
+        if drop_key != None:
+            data = data.dropna(subset=[drop_key])
+        
+        densities = initialized_cities[key]['densities']
+        initialized_cities[key]['area'] = process_zones(
+            data, densities,
+            initialized_cities[key]['requires_area_conversion'],
+            initialized_cities[key]['area_key'],
+            initialized_cities[key]['zone_code_key']
+        )
+        calculate_distributions(densities, initialized_cities[key]['area'])
+        initialized_cities[key]['index'] = round(calculate_zone_index(initialized_cities[key]['population'], densities, WEIGHTS), 2)
+    
+    city_metrics = {}
+    for key in initialized_cities:
+        city_metrics[key] = {
+            'zone_index': initialized_cities[key]['index'],
+            'density_distributions': format_density_distribution(initialized_cities[key])
+        }
+
+    with open('metrics.json', 'w') as file:
+        json.dump(city_metrics, file)
+    
+    print('done!')
+
+main()
---
+++ calculator/cities.py
@@ -0,0 +1,83 @@
+LOW = 'low'
+MED = 'med'
+HIGH = 'high'
+ALT_HIGH = 'alt_high'
+
+def initalize():
+    return {
+        'phoenix': {
+            'densities': {
+                LOW: {'codes': {'S-1', 'R1-10', 'RE-43', 'RE-24', 'R1-14', 'RE-35', 'R1-18', 'R1-10', 'R1-8'}, 'area': 0, 'dist': 0},
+                MED: {'codes': {'R1-6', 'R-2', 'R-3', 'R-3A', 'R-4', 'R-5', 'R-4A'}, 'area': 0, 'dist': 0},
+                HIGH: {'codes': {'UR'}, 'area': 0, 'dist': 0},
+                ALT_HIGH: ('HR', 'DTC')
+            },
+            'population':  1.65e6,
+            'area_key': 'ACRES',
+            'zone_code_key': 'ZONING',
+            'requires_area_conversion': False,
+            'index': 0,
+            'area': 0,
+            'drop': 'ACRES',
+        },
+        'raleigh': {
+            'densities': {
+                LOW: {'codes': {'R-1', 'R-2', 'R-3', 'R-4'}, 'area': 0, 'dist': 0},
+                MED: {'codes': {'R-5', 'R-6', 'RX-3', 'RX-4'}, 'area': 0, 'dist': 0},
+                HIGH: {'codes': {'R-10', 'RX-5', 'RX-7', 'RX-12'}, 'area': 0, 'dist': 0},
+                ALT_HIGH: ('DX')
+            },
+            'population':  4.82e5,
+            'area_key': 'SHAPEAREA',
+            'zone_code_key': 'ZONING',
+            'requires_area_conversion': True,
+            'index': 0,
+            'area': 0,
+            'drop': None
+        },
+        'dallas': {
+            'densities': {
+                LOW: {'codes': {'R-7.5(A)', 'R-7.5(A)', 'R-13(A)', 'R-16(A)' 'R-1/2ac(A)', 'R-1ac(A)', 'MH'}, 'area': 0, 'dist': 0},
+                MED: {'codes': {'D(A)', 'TH-1(A)', 'TH-2(A)', 'TH-3(A)', 'CH'}, 'area': 0, 'dist': 0},
+                HIGH: {'codes': {'MF-1(A)', 'MF-2(A)', 'MF-3(A)', 'MF-4(A)'}, 'area': 0, 'dist': 0},
+                ALT_HIGH: ()
+            },
+            'population': 1.303e6,
+            'area_key': 'Shape__Area',
+            'zone_code_key': 'ZONE_DIST',
+            'requires_area_conversion': True,
+            'index': 0,
+            'area': 0,
+            'drop': None
+        },
+        'los_angeles': {
+            'densities': {
+                LOW: {'codes': {'RA', 'A1', 'RE40', 'RE20', 'RE15', 'RE11', 'RE9', 'RMP'}, 'area': 0, 'dist': 0},
+                MED: {'codes': {'RS', 'R1', 'R1V', 'R1F', 'R1R', 'R1H', 'R2', 'RD3', 'RD4', 'RD5', 'RD6'}, 'area': 0, 'dist': 0},
+                HIGH: {'codes': {'RD1.5', 'RD2', 'RW2', 'R3', 'RAS3', 'R4', 'RAS4', 'R5'}, 'area': 0, 'dist': 0},
+                ALT_HIGH: ()
+            },
+            'population': 3.821e6,
+            'area_key': 'Shape__Area',
+            'zone_code_key': 'ZONE_CLASS',
+            'requires_area_conversion': True,
+            'index': 0,
+            'area': 0,
+            'drop': None
+        },
+        'cincinnati': {
+            'densities': {
+                LOW: {'codes': {'SF-20', 'SF-10'}, 'area': 0, 'dist': 0},
+                MED: {'codes': {'SF-6', 'SF-4'}, 'area': 0, 'dist': 0},
+                HIGH: {'codes': {'SF-2'}, 'area': 0, 'dist': 0},
+                ALT_HIGH: ()
+            },
+            'population': 3.11e5,
+            'area_key': 'ACRES',
+            'zone_code_key': 'ZONING',
+            'requires_area_conversion': False,
+            'index': 0,
+            'area': 0,
+            'drop': None
+        }
+    }
